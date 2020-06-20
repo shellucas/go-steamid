e4cc2cd8cd70f547cf80cb079f82f73555e3c2ad
NemuruYama <nemuruyama@gmail.com>
2020-06-20 13:14:53 +0200

    Fix naming thing

---
+++ steamid/steamid_test.go
@@ -0,0 +1,37 @@
+package steamid_test
+
+import (
+	"testing"
+
+	"github.com/BlackwonderTF/go-steamid/steamid"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSteamIDs(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "SteamID Suite")
+}
+
+var _ = Describe("SteamID", func() {
+
+	Context("STEAM_0:0:458887262", func() {
+		x, err := steamid.CreateSteamID("STEAM_0:0:458887262")
+
+		It("Should not throw an error", func() {
+			Expect(err).To(BeNil())
+		})
+
+		It("SteamID2 should be the same", func() {
+			Expect(x.GetSteam2RenderedID()).To(Equal("STEAM_0:0:458887262"))
+		})
+
+		It("SteamID3 should be converted", func() {
+			Expect(x.GetSteam3RenderedID()).To(Equal("[U:1:917774524]"))
+		})
+
+		It("SteamID64 should be converted", func() {
+			Expect(x.GetSteamID64()).To(Equal("76561198878040252"))
+		})
+	})
+})
